4264b7776879719c305e096f1216bab281ceaf5b
Geraldo Kandonga Fillipus <fillipusgeraldo@gmail.com>
2022-09-28 14:26:04 +0200

    dashboard link

--- modules/Admin/Views/layout/_sidebar.php
+++ modules/Admin/Views/layout/_sidebar.php
@@ -20,6 +20,15 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
+                    <a href="<?php echo base_url('/admin/dashboard'); ?>" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="<?php echo base_url('/admin/categories'); ?>" class="nav-link">
                         <i class="fa fa-list-ul"></i>
                         <p>
